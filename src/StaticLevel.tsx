8f51d27f51be87ce52a6e01023d68b47a1bea7f8
Nick Matantsev <nick.matantsev@gmail.com>
2022-01-16 02:45:40 -0500

    Convert level to use new CSG components

--- src/StaticLevel.tsx
+++ src/StaticLevel.tsx
@@ -5,28 +5,36 @@ import * as THREE from 'three';
 import { Op, Shape, CSGRoot, CSGOp, CSGContent } from './csg';
 import { LevelMesh } from './levelMesh';
 
-// export const Corridor: React.FC<{ color?: string }> = ({ color }) => {
-//   return (
-//     <group>
-//       <Shape type="cuboid" center={[0, 0, 1]} size={[2, 10, 2]} />
+export const Corridor: React.FC<{ color?: string }> = ({ color }) => {
+  return (
+    <group>
+      <CSGContent>
+        <mesh position={[0, 0, 1]}>
+          <boxBufferGeometry args={[2, 10, 2]} />
+        </mesh>
+      </CSGContent>
 
-//       <mesh
-//         position={[0, 0, 2.05]}
-//         rotation={new THREE.Euler(Math.PI, 0, 0)}
-//         receiveShadow
-//       >
-//         <planeGeometry args={[0.5, 0.5]} />
-//         <meshStandardMaterial
-//           color="#202020"
-//           emissive={new THREE.Color(color || '#ffffa0')}
-//           emissiveIntensity={2}
-//         />
+      <mesh
+        position={[0, 0, 2.05]}
+        rotation={new THREE.Euler(Math.PI, 0, 0)}
+        receiveShadow
+      >
+        <planeGeometry args={[0.5, 0.5]} />
+        <meshStandardMaterial
+          color="#202020"
+          emissive={new THREE.Color(color || '#ffffa0')}
+          emissiveIntensity={2}
+        />
 
-//         <Shape type="cuboid" center={[0, 0, 0]} size={[0.5, 0.5, 0.1]} />
-//       </mesh>
-//     </group>
-//   );
-// };
+        <CSGContent>
+          <mesh>
+            <boxBufferGeometry args={[0.5, 0.5, 0.1]} />
+          </mesh>
+        </CSGContent>
+      </mesh>
+    </group>
+  );
+};
 
 export const StaticLevel: React.FC = () => {
   const rampMatrix = new THREE.Matrix4();
@@ -35,49 +43,35 @@ export const StaticLevel: React.FC = () => {
   return (
     <>
       <LevelMesh>
-        <CSGOp type="union">
-          <group matrix={rampMatrix} matrixAutoUpdate={false}>
-            <CSGContent>
-              <mesh position={[-2, 1, 1.5]}>
-                <boxBufferGeometry args={[4, 2, 3]} />
-              </mesh>
-            </CSGContent>
-          </group>
-
+        <group matrix={rampMatrix} matrixAutoUpdate={false}>
           <CSGContent>
-            <mesh position={[0, 1, 1.5]}>
-              <boxBufferGeometry args={[1, 2, 1]} />
+            <mesh position={[-2, 1, 1.5]}>
+              <boxBufferGeometry args={[4, 2, 3]} />
             </mesh>
           </CSGContent>
-        </CSGOp>
+        </group>
 
-        {/*<Op type="union">*/}
-        {/*<group matrix={rampMatrix} matrixAutoUpdate={false}>
-            <Shape type="cuboid" center={[-2, 1, 1.5]} size={[4, 2, 3]} />
-          </group>*/}
-
-        {/*<group position={[1, 1, 0]}>
-            <Corridor />
-          </group>
-          <group position={[1, 9, 0]}>
-            <Corridor />
-          </group>
-          <group position={[9, 1, 0]}>
-            <Corridor color="#000000" />
-          </group>
-          <group position={[9, 9, 0]}>
-            <Corridor color="#000000" />
-          </group>
-          <group position={[5, -3, 0]} rotation={[0, 0, Math.PI / 2]}>
-            <Corridor />
-          </group>
-          <group position={[5, 5, 0]} rotation={[0, 0, Math.PI / 2]}>
-            <Corridor color="#c08000" />
-          </group>
-          <group position={[5, 13, 0]} rotation={[0, 0, Math.PI / 2]}>
-            <Corridor color="#000000" />
-          </group>*/}
-        {/*</Op>*/}
+        <group position={[1, 1, 0]}>
+          <Corridor />
+        </group>
+        <group position={[1, 9, 0]}>
+          <Corridor />
+        </group>
+        <group position={[9, 1, 0]}>
+          <Corridor color="#000000" />
+        </group>
+        <group position={[9, 9, 0]}>
+          <Corridor color="#000000" />
+        </group>
+        <group position={[5, -3, 0]} rotation={[0, 0, Math.PI / 2]}>
+          <Corridor />
+        </group>
+        <group position={[5, 5, 0]} rotation={[0, 0, Math.PI / 2]}>
+          <Corridor color="#c08000" />
+        </group>
+        <group position={[5, 13, 0]} rotation={[0, 0, Math.PI / 2]}>
+          <Corridor color="#000000" />
+        </group>
       </LevelMesh>
 
       <pointLight color="#f0f0ff" position={[6.5, 12.5, 0.25]} castShadow />
