45a9e813f1dc061db85c65d9e123703f8cf5db16
Nick Matantsev <nick.matantsev@gmail.com>
2022-01-23 20:03:31 -0500

    Improve performance a bit

--- src/level/UtilityCorridor.tsx
+++ src/level/UtilityCorridor.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as THREE from 'three';
+import { LightmapReadOnly } from '@react-three/lightmap';
 
 import { CSGContent } from '../csg';
 import { LevelMesh, WorldUV } from '../levelMesh';
@@ -23,32 +24,34 @@ export const UtilityCorridor: React.FC<{ color?: string }> = ({ color }) => {
         </mesh>
       </CSGContent>
 
-      <mesh
-        position={[0, 0, 2.249]}
-        rotation={new THREE.Euler(Math.PI, 0, 0)}
-        receiveShadow
-      >
-        <planeBufferGeometry args={[0.25, 0.5]} />
-        <meshStandardMaterial
-          color="#202020"
-          emissive={new THREE.Color(color || '#ffffe0')}
-          emissiveIntensity={1.2}
-        />
-      </mesh>
-      <mesh position={[0, 0, 2.275]} receiveShadow>
-        <boxBufferGeometry args={[0.35, 0.6, 0.05]} />
-        <meshStandardMaterial color="#808080" />
-
-        {/* support stem going up */}
+      <LightmapReadOnly>
         <mesh
-          position={[0, 0, 0.5]}
+          position={[0, 0, 2.249]}
           rotation={new THREE.Euler(Math.PI, 0, 0)}
           receiveShadow
         >
-          <boxBufferGeometry args={[0.05, 0.05, 1]} />
-          <meshStandardMaterial color="#808080" />
+          <planeBufferGeometry args={[0.25, 0.5]} />
+          <meshStandardMaterial
+            color="#202020"
+            emissive={new THREE.Color(color || '#ffffe0')}
+            emissiveIntensity={1.2}
+          />
         </mesh>
-      </mesh>
+        <mesh position={[0, 0, 2.275]} receiveShadow>
+          <boxBufferGeometry args={[0.35, 0.6, 0.05]} />
+          <meshStandardMaterial color="#808080" />
+
+          {/* support stem going up */}
+          <mesh
+            position={[0, 0, 0.5]}
+            rotation={new THREE.Euler(Math.PI, 0, 0)}
+            receiveShadow
+          >
+            <boxBufferGeometry args={[0.05, 0.05, 1]} />
+            <meshStandardMaterial color="#808080" />
+          </mesh>
+        </mesh>
+      </LightmapReadOnly>
     </group>
   );
 };
