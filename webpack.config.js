16394bb01728bc68023ebb9c5bb14e93f0828158
Nick Matantsev <nick.matantsev@gmail.com>
2022-01-25 02:51:08 -0500

    Add assets to build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const ASSETS_DIR =
+  'C:\\Users\\nickm\\Google Drive\\Gamedev\\Sector3f Public Assets';
 
 module.exports = {
   entry: {
@@ -9,7 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name]_bundle.[hash].js'
+    filename: '[name]_bundle.[contenthash].js'
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx']
@@ -29,14 +34,25 @@ module.exports = {
       chunks: ['index'],
       filename: 'index.html',
       template: 'src/index.html'
+    }),
+
+    new CleanWebpackPlugin(),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: ASSETS_DIR,
+          to: 'assets'
+          // globOptions: { ignore: ['**/desktop.ini'] }
+        }
+      ]
     })
   ],
 
   devServer: {
     static: [
       {
-        directory:
-          'C:\\Users\\nickm\\Google Drive\\Gamedev\\Sector3f Public Assets',
+        directory: ASSETS_DIR,
         publicPath: '/assets'
       }
     ]
